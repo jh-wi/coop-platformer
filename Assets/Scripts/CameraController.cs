00dabdcc7983a14bc3acdd75e093ebb4c0cba1f6
Amanda Nelson <anelson99@ku.edu>
2022-02-08 16:01:00 -0600

    Made controller follow baby bird and made level longer with start and end markers

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    private void FixedUpdate()
+    {
+        transform.position = target.position+offset;
+    }
+}
