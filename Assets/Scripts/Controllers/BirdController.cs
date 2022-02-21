07a7b124c1c5fc409bc34998157e5520bd82efcd
kodiakwhale <bearbeluga@gmail.com>
2022-02-21 10:05:43 -0600

    added a few unfinished scripts

---
+++ Assets/Scripts/Controllers/BirdController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+	
+	Vector3 playerVelocity;
+	bool grounded;
+	float moveSpeed;
+	float jumpForce;
+	
+	void PlayerMove() {
+		
+	}
+	
+	void Update() {
+		PlayerMove();
+	}
+	
+}
---
+++ Assets/Scripts/Keybinds.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Keybinds {
+	
+	public static KeyCode bearJump = KeyCode.W;
+	public static KeyCode bearLeft = KeyCode.A;
+	public static KeyCode bearDown = KeyCode.S;
+	public static KeyCode bearRight = KeyCode.D;
+	
+	public static KeyCode birdJump = KeyCode.UpArrow;
+	public static KeyCode birdLeft = KeyCode.LeftArrow;
+	public static KeyCode birdDown = KeyCode.DownArrow;
+	public static KeyCode birdRight = KeyCode.RightArrow;
+	
+	public static void ResetControls() {
+		bearJump = KeyCode.W;
+		bearJump = KeyCode.A;
+		bearJump = KeyCode.S;
+		bearJump = KeyCode.D;
+		
+		birdJump = KeyCode.UpArrow;
+		birdJump = KeyCode.LeftArrow;
+		birdJump = KeyCode.DownArrow;
+		birdJump = KeyCode.RightArrow;
+	}
+	
+}
